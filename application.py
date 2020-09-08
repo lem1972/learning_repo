22d4be3b83ea9eab066d4ad604d9740a437b2783
lem1972 <67788863+lem1972@users.noreply.github.com>
2020-09-08 16:36:59 -0300

    Update application.py

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<html><body><h1>Hello Best Bike App!</h1></body></html>\n"
+    return "<html><body><h1>Hello World! First App de LEM</h1></body></html>\n"
 
 if __name__ == "__main__":
     app.run(port=5000)
